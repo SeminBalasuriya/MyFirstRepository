b19cbc1240ac846332cbd1af7692e34ceed32245
Semin Balasuriya <106874258+SeminBalasuriya@users.noreply.github.com>
2022-06-08 15:53:42 +0530

    This is the first C code uploaded to the repository

---
+++ git hub.c
@@ -0,0 +1,9 @@
+// my first repository
+
+#include <stdio.h>
+int main (void) // function main begins program execution 
+{
+	printf("My first repository\n"); //prompt
+		
+	return 0; // end main function
+}
